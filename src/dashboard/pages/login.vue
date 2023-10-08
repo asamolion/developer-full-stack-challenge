83802445efe268e47c7319c24b3440d44bbead4f
Muhammad Osama Arshad <muhammadosamaarshad@gmail.com>
2023-10-08 21:34:23 +0500

    docs: update readme with challenge solution

--- src/dashboard/pages/login.vue
+++ src/dashboard/pages/login.vue
@@ -33,8 +33,8 @@ export default {
     data() {
         return {
             form: {
-                username: '',
-                password: '',
+                username: 'osama',
+                password: 'Lahore22',
             },
         };
     },
